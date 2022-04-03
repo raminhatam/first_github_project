62a6f0fc2ed72fd6210e131cd5ca11ca08ef461c
ASUS <raminhm605@gmail.com>
2022-04-03 10:36:19 +0430

    initial commit

---
+++ app/src/main/java/ir/hm/firstgithubproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package ir.hm.firstgithubproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "fist guthub project", Toast.LENGTH_SHORT).show()
+
+    }
+}
